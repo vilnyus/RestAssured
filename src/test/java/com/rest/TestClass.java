bcdfe9f18bf5cd9f3eb7cdc2128da0ebc1423020
vagharsh <govagharsh@gmail.com>
2021-08-07 23:42:05 +0400

    Specification Builder added in @BeforeClass

--- src/test/java/com/rest/TestClass.java
+++ src/test/java/com/rest/TestClass.java
@@ -1,6 +1,8 @@
 package com.rest;
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.config.LogConfig;
+import io.restassured.filter.log.LogDetail;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.testng.annotations.BeforeClass;
@@ -18,16 +20,28 @@ import static org.hamcrest.Matchers.*;
 public class TestClass {
     RequestSpecification requestSpecification;
 
-    @BeforeClass
-    public void beforeClass(){
-        requestSpecification = with().
-                baseUri("https://api.postman.com").
-                header("X-Api-Key", "PMAK-610294b97cf9bc003484e6ef-bbee37fbc8767f08d1db6aaedd430829d0").
-                log().all();
+//    @BeforeClass()
+//    public void beforeClass(){
+//        requestSpecification = with().
+//                baseUri("https://api.postman.com").
+//                header("X-Api-Key", "PMAK-610294b97cf9bc003484e6ef-bbee37fbc8767f08d1db6aaedd430829d0").
+//                log().all();
+//    }
+
+    @BeforeClass()
+    public void beforeClassBuilderStyle(){
+        RequestSpecBuilder specificationBuilder = new RequestSpecBuilder();
+
+        specificationBuilder.setBaseUri("https://api.postman.com");
+        specificationBuilder.addHeader("X-Api-Key", "PMAK-610294b97cf9bc003484e6ef-bbee37fbc8767f08d1db6aaedd430829d0");
+        specificationBuilder.addHeader("My_Header", "my_header_value"); //Custome header
+        specificationBuilder.log(LogDetail.ALL);
+
+        requestSpecification = specificationBuilder.build();
     }
+
     @Test
     public void BlackListHeaders(){
-
         Set<String> headers = new HashSet<>();
         headers.add("X-Api-Key");
         headers.add("Accept");
@@ -69,7 +83,7 @@ public class TestClass {
 
     @Test
     public void non_bdd_style_validation(){
-        Response response = requestSpecification.get("/workspaces");
+        Response response = given().spec(requestSpecification).get("/workspaces");
         assertThat(response.statusCode(), is(equalTo(200)));
         assertThat(response.path("workspaces[0].name"), is(equalTo("My Test Workspace")));
     }
